7e1f7145e1477b98c7f66638e972563671a81bba
Thomas Hodges <thodges@javascriptonline.org>
2023-02-07 20:28:55 -0800

    useRef basics

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,8 +1,14 @@
+import { useRef } from "react";
+
 const Box = (props) => {
+  const instanceRef = useRef();
+  const materialRef = useRef();
+  console.log(instanceRef);
+  console.log(materialRef);
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={instanceRef}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshBasicMaterial color={0x00ff00} wireframe ref={materialRef} />
     </mesh>
   );
 };
