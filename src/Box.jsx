522312c08ca2879df3ce1cb10091b2838971d256
Thomas Hodges <thodges@javascriptonline.org>
2023-02-07 20:54:16 -0800

    basic animation

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,9 +1,15 @@
 import { useRef, useEffect } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const Box = (props) => {
   const ref = useRef();
 
-  useEffect(() => console.log(ref));
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.05 * delta;
+  });
+
+  // useEffect(() => console.log(ref));
 
   return (
     <mesh {...props} ref={ref}>
