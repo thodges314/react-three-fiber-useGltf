ba7d9965b38ce18f61ad01a276fa6b5d162df1c2
Thomas Hodges <thodges@javascriptonline.org>
2023-02-07 23:39:43 -0800

    leva controls SO AWESOM!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Stats, OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 import { BoxGeometry, SphereGeometry, DodecahedronGeometry } from "three";
 import Polyhedron from "./Polyhedron";
 
@@ -9,12 +11,37 @@ const App = () => {
     new SphereGeometry(0.785398),
     new DodecahedronGeometry(0.785398),
   ];
+
+  const options = useMemo(
+    () => ({
+      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      visible: true,
+      color: { value: "lime" },
+    }),
+    []
+  );
+
+  const pA = useControls("Polyhedron A", options);
+  const pB = useControls("Polyhedron B", options);
   return (
     <Canvas camera={{ position: [0, 0, 3] }}>
-      <Polyhedron position={[-0.75, -0.75, 0]} polyhedreon={polyhedron} />
-      <Polyhedron position={[0.75, -0.75, 0]} polyhedreon={polyhedron} />
-      <Polyhedron position={[-0.75, 0.75, 0]} polyhedreon={polyhedron} />
-      <Polyhedron position={[0.75, 0.75, 0]} polyhedreon={polyhedron} />
+      <Polyhedron
+        position={[-1, 1, 0]}
+        rotation={[pA.x, pA.y, pA.z]}
+        visible={pA.visible}
+        color={pA.color}
+        polyhedreon={polyhedron}
+      />
+      <Polyhedron
+        position={[1, 1, 0]}
+        rotation={[pB.x, pB.y, pB.z]}
+        visible={pB.visible}
+        color={pB.color}
+        polyhedreon={polyhedron}
+      />
+
       <OrbitControls />
       <axesHelper args={[5]} />
       <gridHelper />
--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,23 +1,17 @@
 import { useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
 
-const Polyhedron = ({ position, polyhedreon }) => {
+const Polyhedron = ({ polyhedreon, color, ...props }) => {
   const ref = useRef();
   const [count, setCount] = useState(0);
 
-  useFrame((_, delta) => {
-    ref.current.rotation.x += delta;
-    ref.current.rotation.y += 0.5 * delta;
-  });
-
   return (
     <mesh
+      {...props}
       ref={ref}
-      position={position}
       onPointerDown={() => setCount((count + 1) % 3)}
       geometry={polyhedreon[count]}
     >
-      <meshBasicMaterial color={"lime"} wireframe />
+      <meshBasicMaterial color={color} wireframe />
     </mesh>
   );
 };
