2be4173ba03db3ae7dcbc4f494cf442e3dfa71bc
Thomas Hodges <thodges@javascriptonline.org>
2023-02-08 19:57:29 -0800

    environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-import {
-  Circle,
-  OrbitControls,
-  Stats,
-} from '@react-three/drei';
-import {
-  Canvas,
-  useLoader,
-} from '@react-three/fiber';
+import { Environment, OrbitControls, Stats } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
 
 const App = () => {
-  const gltf = useLoader(GLTFLoader, "./models/monkey.glb");
+  const gltf = useLoader(GLTFLoader, "./models/monkeyShine.glb");
   return (
     <Canvas shadows camera={{ position: [-0.5, 1, 2] }}>
-      <directionalLight position={[3.3, 1.0, 4.4]} castShadow />
+      <Environment preset="sunset" background blur={0.05} />
+      {/* <directionalLight position={[3.3, 1.0, 4.4]} /> */}
       <primitive
         object={gltf.scene}
         position={[0, 1, 0]}
         children-0-castShadow
       />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
+
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
