a0c36735f40eeaec3ef8768665beb88db852479f
Thomas Hodges <thodges@javascriptonline.org>
2023-02-08 16:44:54 -0800

    lights

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,130 @@
-import { Canvas } from "@react-three/fiber";
-import { Stats, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+
+import { useControls } from "leva";
 import {
+  Color,
   MeshBasicMaterial,
   MeshNormalMaterial,
   MeshPhongMaterial,
   MeshStandardMaterial,
 } from "three";
+
+import { OrbitControls, Stats } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 import Polyhedron from "./Polyhedron";
+
+const Lights = () => {
+  const ambientRef = useRef();
+  const directionalRef = useRef();
+  const pointRef = useRef();
+  const spotRef = useRef();
+
+  useControls("Ambient Light", {
+    visible: {
+      value: false,
+      onChange: (v) => (ambientRef.current.visible = v),
+    },
+    color: {
+      value: "white",
+      onChange: (v) => (ambientRef.current.color = new Color(v)),
+    },
+  });
+
+  useControls("Directional Light", {
+    visible: {
+      value: true,
+      onChange: (v) => (directionalRef.current.visible = v),
+    },
+    position: {
+      x: 1,
+      y: 1,
+      z: 1,
+      onChange: (v) => directionalRef.current.position.copy(v),
+    },
+    color: {
+      value: "white",
+      onChange: (v) => (directionalRef.current.color = new Color(v)),
+    },
+  });
+
+  useControls("Point Light", {
+    visible: {
+      value: false,
+      onChange: (v) => (pointRef.current.visible = v),
+    },
+    position: {
+      x: 2,
+      y: 0,
+      z: 0,
+      onChange: (v) => pointRef.current.position.copy(v),
+    },
+    color: {
+      value: "white",
+      onChange: (v) => (pointRef.current.color = new Color(v)),
+    },
+  });
+
+  useControls("Spot Light", {
+    visible: {
+      value: false,
+      onChange: (v) => (spotRef.current.visible = v),
+    },
+    position: {
+      x: 3,
+      y: 2.5,
+      z: 2,
+      onChange: (v) => spotRef.current.position.copy(v),
+    },
+    color: {
+      value: "white",
+      onChange: (v) => (spotRef.current.color = new Color(v)),
+    },
+  });
+  return (
+    <>
+      <ambientLight ref={ambientRef} />
+      <directionalLight ref={directionalRef} />
+      <pointLight ref={pointRef}>
+        <mesh>
+          <sphereGeometry args={[0.25]} />
+        </mesh>
+      </pointLight>
+      <spotLight ref={spotRef} />
+    </>
+  );
+};
 
 const App = () => {
   return (
-    <Canvas camera={{ position: [-1, 4, 2.5] }}>
-      <directionalLight position={[1, 1, 1]} />
+    <Canvas camera={{ position: [4, 4, 1.5] }}>
+      <Lights />
       <Polyhedron
         name="meshBasicMaterial"
         position={[-3, 1, 0]}
-        material={new MeshBasicMaterial()}
+        material={new MeshBasicMaterial({ color: "yellow" })}
       />
       <Polyhedron
         name="meshNormalMaterial"
         position={[-1, 1, 0]}
-        material={new MeshNormalMaterial()}
+        material={new MeshNormalMaterial({ flatShading: true })}
       />
       <Polyhedron
         name="meshPhongMaterial"
         position={[1, 1, 0]}
-        material={new MeshPhongMaterial()}
+        material={new MeshPhongMaterial({ color: "lime", flatShading: true })}
       />
       <Polyhedron
         name="meshStandardMaterial"
         position={[3, 1, 0]}
-        material={new MeshStandardMaterial()}
+        material={
+          new MeshStandardMaterial({
+            color: 0xff0033,
+            flatShading: true,
+          })
+        }
       />
-      <OrbitControls target-y={1} />
+      <OrbitControls target={[2, 2, 0]} />
       <axesHelper args={[5]} />
       <gridHelper />
       <Stats />
--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,35 +1,12 @@
-import { useRef } from "react";
-import { useControls } from "leva";
-import { Color } from "three";
-import { useFrame } from "@react-three/fiber";
+import { useRef } from 'react';
+
+import { useFrame } from '@react-three/fiber';
 
 const Polyhedron = (props) => {
   const ref = useRef(); // ref defined here and used in poth controls and to attach to mesh
   useFrame((_, delta) => {
     ref.current.rotation.x += 0.2 * delta;
     ref.current.rotation.y += 0.05 * delta;
-  });
-
-  useControls(props.name, {
-    wireframe: {
-      value: false,
-      onChange: (v) => {
-        ref.current.material.wireframe = v;
-      },
-    },
-    flatShading: {
-      value: true,
-      onChange: (v) => {
-        ref.current.material.flatShading = v;
-        ref.current.material.needsUpdate = true;
-      },
-    },
-    color: {
-      value: "lime",
-      onChange: (v) => {
-        ref.current.material.color = new Color(v);
-      },
-    },
   });
 
   return (
