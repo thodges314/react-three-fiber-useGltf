cf455b82fa2df2b26752aef1cd69b2eb219ce381
Thomas Hodges <thodges@javascriptonline.org>
2023-02-09 16:45:35 -0800

    more controls

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,60 @@ const Env = () => {
   );
 };
 
-const App = () => {
-  const gltf = useLoader(GLTFLoader, "./models/scene.glb");
+const Model = () => {
+  const { scene } = useLoader(GLTFLoader, "./models/scene.glb");
+
+  const {
+    x,
+    y,
+    z,
+    visible,
+    color,
+    metalness,
+    roughness,
+    clearcoat,
+    clearcoatRoughness,
+    transmission,
+    ior,
+    thickness,
+  } = useControls("Suzanne", {
+    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    visible: true,
+    color: { value: "#ffbc85" },
+    metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
+    clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
+    ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
+    thickness: { value: 3.12, min: 0, max: 5, step: 0.01 },
+  });
+
   return (
-    <Canvas shadows camera={{ position: [-0.5, 1, 2] }}>
+    <primitive
+      object={scene}
+      children-0-rotation={[x, y, z]}
+      children-0-visible={visible}
+      children-0-material-color={color}
+      children-0-material-metalness={metalness}
+      children-0-material-roughness={roughness}
+      children-0-material-clearcoat={clearcoat}
+      children-0-material-clearcoatRoughness={clearcoatRoughness}
+      children-0-material-transmission={transmission}
+      children-0-material-ior={ior}
+      children-0-material-thickness={thickness}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas shadows camera={{ position: [-8, 5, 8], fov: 40 }}>
       <Env />
-      <primitive object={gltf.scene} />
-      <OrbitControls target={[0, 1, 0]} />
+      <Model />
+      <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
       <axesHelper args={[5]} />
       <Stats />
     </Canvas>
