a710168d0ca72a026a5a386e39543d7f68bdc71a
Thomas Hodges <thodges@javascriptonline.org>
2023-02-09 16:33:24 -0800

    cleanup and usecontrols for scene

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,30 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Environment, OrbitControls, Stats } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 
+import { useControls } from "leva";
+
+const Env = () => {
+  const { height, radius, scale } = useControls("Ground", {
+    height: { value: 10, min: 0, max: 100, step: 1 },
+    radius: { value: 115, min: 0, max: 1000, step: 1 },
+    scale: { value: 100, min: 0, max: 1000, step: 1 },
+  });
+  return (
+    <Environment
+      files="./img/shanghai_bund_1k.hdr"
+      background
+      ground={{ height: height, radius: radius, scale: scale }}
+    />
+  );
+};
+
 const App = () => {
-  const gltf = useLoader(GLTFLoader, "./models/monkeyShine.glb");
+  const gltf = useLoader(GLTFLoader, "./models/scene.glb");
   return (
     <Canvas shadows camera={{ position: [-0.5, 1, 2] }}>
-      <Environment
-        files="./img/dikhololo_night_1k.hdr"
-        background
-        blur={0.05}
-      />
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
-      <primitive object={gltf.scene} position={[0, 1, 0]} />
-
-      <OrbitControls target={[0, 1, 0]} autoRotate />
+      <Env />
+      <primitive object={gltf.scene} />
+      <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
     </Canvas>
